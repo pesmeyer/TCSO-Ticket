2c40d648dc18350f2510193ebff7770fc9015062
Unknown <unknown@example.com>
2019-10-25 10:48:41 -0500

    Added base program

---
+++ SampleTicketer/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace SampleTicketer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSupport = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnStatistics = new System.Windows.Forms.Button();
+            this.btnCompletedTickets = new System.Windows.Forms.Button();
+            this.btnMyTicket = new System.Windows.Forms.Button();
+            this.btnNewTicket = new System.Windows.Forms.Button();
+            this.btnOverview = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblHello = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Controls.Add(this.btnSupport);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.btnStatistics);
+            this.panel1.Controls.Add(this.btnCompletedTickets);
+            this.panel1.Controls.Add(this.btnMyTicket);
+            this.panel1.Controls.Add(this.btnNewTicket);
+            this.panel1.Controls.Add(this.btnOverview);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(201, 562);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnSupport
+            // 
+            this.btnSupport.FlatAppearance.BorderSize = 0;
+            this.btnSupport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSupport.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSupport.Location = new System.Drawing.Point(0, 417);
+            this.btnSupport.Name = "btnSupport";
+            this.btnSupport.Size = new System.Drawing.Size(201, 54);
+            this.btnSupport.TabIndex = 7;
+            this.btnSupport.Text = "Support";
+            this.btnSupport.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 530);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 23);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Version 0.0.1";
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(0, 308);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(201, 54);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Purchase Requests";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnStatistics
+            // 
+            this.btnStatistics.FlatAppearance.BorderSize = 0;
+            this.btnStatistics.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStatistics.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnStatistics.Location = new System.Drawing.Point(0, 363);
+            this.btnStatistics.Name = "btnStatistics";
+            this.btnStatistics.Size = new System.Drawing.Size(201, 54);
+            this.btnStatistics.TabIndex = 6;
+            this.btnStatistics.Text = "Statistics";
+            this.btnStatistics.UseVisualStyleBackColor = true;
+            // 
+            // btnCompletedTickets
+            // 
+            this.btnCompletedTickets.FlatAppearance.BorderSize = 0;
+            this.btnCompletedTickets.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCompletedTickets.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnCompletedTickets.Location = new System.Drawing.Point(0, 254);
+            this.btnCompletedTickets.Name = "btnCompletedTickets";
+            this.btnCompletedTickets.Size = new System.Drawing.Size(201, 54);
+            this.btnCompletedTickets.TabIndex = 4;
+            this.btnCompletedTickets.Text = "Completed Tickets";
+            this.btnCompletedTickets.UseVisualStyleBackColor = true;
+            // 
+            // btnMyTicket
+            // 
+            this.btnMyTicket.FlatAppearance.BorderSize = 0;
+            this.btnMyTicket.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMyTicket.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnMyTicket.Location = new System.Drawing.Point(0, 206);
+            this.btnMyTicket.Name = "btnMyTicket";
+            this.btnMyTicket.Size = new System.Drawing.Size(201, 54);
+            this.btnMyTicket.TabIndex = 3;
+            this.btnMyTicket.Text = "My Tickets";
+            this.btnMyTicket.UseVisualStyleBackColor = true;
+            // 
+            // btnNewTicket
+            // 
+            this.btnNewTicket.FlatAppearance.BorderSize = 0;
+            this.btnNewTicket.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNewTicket.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnNewTicket.Location = new System.Drawing.Point(0, 153);
+            this.btnNewTicket.Name = "btnNewTicket";
+            this.btnNewTicket.Size = new System.Drawing.Size(201, 54);
+            this.btnNewTicket.TabIndex = 2;
+            this.btnNewTicket.Text = "New Ticket";
+            this.btnNewTicket.UseVisualStyleBackColor = true;
+            // 
+            // btnOverview
+            // 
+            this.btnOverview.FlatAppearance.BorderSize = 0;
+            this.btnOverview.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOverview.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnOverview.Location = new System.Drawing.Point(0, 99);
+            this.btnOverview.Name = "btnOverview";
+            this.btnOverview.Size = new System.Drawing.Size(201, 54);
+            this.btnOverview.TabIndex = 1;
+            this.btnOverview.Text = "Overview";
+            this.btnOverview.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(99)))), ((int)(((byte)(116)))));
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(201, 100);
+            this.panel3.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Gill Sans MT", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(32, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 33);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "TCSO TIKET";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(201, 311);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(773, 251);
+            this.panel2.TabIndex = 1;
+            // 
+            // lblHello
+            // 
+            this.lblHello.AutoSize = true;
+            this.lblHello.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblHello.Location = new System.Drawing.Point(220, 18);
+            this.lblHello.Name = "lblHello";
+            this.lblHello.Size = new System.Drawing.Size(55, 23);
+            this.lblHello.TabIndex = 4;
+            this.lblHello.Text = "Hello, ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ClientSize = new System.Drawing.Size(974, 562);
+            this.Controls.Add(this.lblHello);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Gill Sans MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TCSO Tiket";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnStatistics;
+        private System.Windows.Forms.Button btnCompletedTickets;
+        private System.Windows.Forms.Button btnMyTicket;
+        private System.Windows.Forms.Button btnNewTicket;
+        private System.Windows.Forms.Button btnOverview;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnSupport;
+        private System.Windows.Forms.Label lblHello;
+    }
+}
+
